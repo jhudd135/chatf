8d6d7cee7a763fe2305448f5f9f0f6b88ac47de1
jhudd135 <jhudd135@gmail.com>
2024-12-19 18:24:49 -0500

    shuffle around the join code

--- src/api.ts
+++ src/api.ts
@@ -6,39 +6,44 @@ import { User } from "./user.ts";
 export function requestHandler(command: string, req: http.IncomingMessage, res: http.ServerResponse) {
     switch (command) {
         case "join":
-            requestBody(req).then(body => {
-                const json: {room: string, user: string, token: string} = JSON.parse(body);
-                if (json.user.length < 4 || json.user.match(/[^\w]/)) {
+            requestBody(req).then(bodyString => {
+                const body: { room: string, user: string, token: string } = JSON.parse(bodyString);
+                if (body.user.length < 4 || body.user.match(/[^\w]/)) {
                     res.writeHead(400);
                     res.end("username must be at least 4 characters long and only contain a-z, A-Z, 0-9, and _");
                     return;
                 }
-                if (rooms.has(json.room)) {
-                    const room: Room = rooms.get(json.room);
+                if (rooms.has(body.room)) {
+                    const room: Room = rooms.get(body.room);
                     let user: User;
-                    if (json.token) {
-                        user = room.userJoin(json.user, json.token);
+                    if (body.token) {
+                        user = room.userJoin(body.user, body.token);
                         if (!user) {
                             res.writeHead(400);
                             res.end("username and token combination does not exist");
-                            return;
+                        } else {
+                            res.setHeader("Content-Type", "application/json");
+                            res.writeHead(200);
+                            res.end(JSON.stringify(user));
                         }
                     } else {
-                        user = room.newUserJoin(json.user);
+                        user = room.newUserJoin(body.user);
                         if (!user) {
                             res.writeHead(409);
                             res.end("username already taken");
-                            return;
+                        } else {
+                            res.setHeader("Content-Type", "application/json");
+                            res.writeHead(201);
+                            res.end(JSON.stringify(user));
                         }
                     }
-                    res.setHeader("Content-Type", "application/json");
-                    res.writeHead(201);
-                    res.end(JSON.stringify(user));
                 } else {
                     res.writeHead(404);
                     res.end("room not found");
                 }
             });
             break;
+        case "leave":
+            requestBody(req).then()
     }
 }
